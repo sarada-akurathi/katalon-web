9e92c0fbaca9802ecb2303231fb1354e090e5599
Sarada Akurathi <saradaakurathi@gmail.com>
2024-01-12 19:27:52 +0530

    login negative testcase

---
+++ Test Suites/DemoBlaze/Login Negative Test Suite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Negative Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>623798d2-86f4-40c3-af86-b0de8e28438d</testSuiteGuid>
+   <testCaseLink>
+      <guid>6cebc01d-5b35-46b5-ac2b-4fd3697971bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoBlaze/Login Negative</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>a29b5963-625e-40bc-9d41-c4f464c9f1f8</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DemoBlaze/Login Negative</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>a29b5963-625e-40bc-9d41-c4f464c9f1f8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>6e4d59f6-2272-41e2-82fb-cd0f4bbdd19b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>a29b5963-625e-40bc-9d41-c4f464c9f1f8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>bb0d45d9-93bc-4bde-89ff-9e439426940c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
