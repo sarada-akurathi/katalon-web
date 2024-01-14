6e707e829632a5170805e30522cfcc85bb6e48ea
Sarada Akurathi <saradaakurathi@gmail.com>
2024-01-14 18:45:51 +0530

    create product details test suite

---
+++ Test Suites/DemoBlaze/Product Details Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Product Details Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>80f9f891-0e35-4b69-a648-9a4700303209</testSuiteGuid>
+   <testCaseLink>
+      <guid>849b933d-5852-4302-95b8-04576a83fb42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoBlaze/Verify Product Details</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
